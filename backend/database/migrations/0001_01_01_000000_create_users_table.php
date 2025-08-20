00fd7e898fe6d0c34e4752d30dcdee08ebd5f0c4
Freeluncher <gandhisatriadewa06@gmail.com>
2025-08-20 09:58:50 +0700

    Add username, role, and photo fields to User model and migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->enum('role',['admin', 'satpam'])->default('satpam');
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
