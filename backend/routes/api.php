0f953e5af21c6b018e894e6c8f689fa551af3564
Freeluncher <gandhisatriadewa06@gmail.com>
2025-08-20 13:20:18 +0700

    Add CheckUserRole middleware and apply it to admin routes for authorization

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,6 +9,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Protected Routes only for Admin
+Route::middleware(['auth:sanctum', 'checkUserRole:admin'])->group(function () {
+
+});
