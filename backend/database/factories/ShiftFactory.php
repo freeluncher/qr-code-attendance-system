cd742455992f09e6a6eaa41d58bdccf2e47d1d27
Freeluncher <gandhisatriadewa06@gmail.com>
2025-08-20 10:39:17 +0700

    Add Shift factory and seeder for generating shift data

---
+++ backend/database/factories/ShiftFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shift>
+ */
+class ShiftFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $shifts = [
+            ['name' => 'Pagi', 'start_time' => '06:00:00', 'end_time' => '14:00:00'],
+            ['name' => 'Siang', 'start_time' => '14:00:00', 'end_time' => '22:00:00'],
+            ['name' => 'Malam', 'start_time' => '22:00:00', 'end_time' => '06:00:00'],
+        ];
+        return $this->faker->randomElement($shifts);
+    }
+}
