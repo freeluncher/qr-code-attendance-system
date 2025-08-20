00e3de0a74415839ff2aa6e5b9fe526b9f871ebb
Freeluncher <gandhisatriadewa06@gmail.com>
2025-08-20 11:53:57 +0700

    Add authentication routes for login and logout in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+// Authentication Routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
